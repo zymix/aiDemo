be905ec4c7eaf899e4c3e92a802d442676aee5ef
zymix <xiankang.he@foxmail.com>
2020-05-30 17:48:36 +0800

    修正人物移动的控制

---
+++ Assets/Scripts/Cameras/CameraFollow.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    float distance = 5f;
+    float rotationSpeed = 90f;
+    float lastManualRotationTime = 0f;
+    Vector3 lastFocusPosition;
+
+    [SerializeField, Range(-89f, 89f)]
+    float maxRotateAngleX = 45f, minRotateAngleX = 15f;
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+    [SerializeField]
+    Transform focus = default;
+    [SerializeField, Min(0f)]
+    float alignDelay = 5f;
+    [SerializeField, Range(0f, 90f)]
+    float alignSmoothRange = 45f;
+
+    Camera cam;
+    Vector2 orbitAngles;
+    Vector3 focusPoint;
+    void Awake() {
+        cam = GetComponent<Camera>();
+        focusPoint = focus.position;
+        orbitAngles = new Vector2( (maxRotateAngleX + minRotateAngleX) / 2.0f, 0f);
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+        lastManualRotationTime = Time.unscaledTime;
+    }
+
+    void OnValidate() {
+        if (maxRotateAngleX < minRotateAngleX) {
+            minRotateAngleX = maxRotateAngleX;
+        }
+    }
+
+    void LateUpdate() {
+        UpdateFocusPoint();
+        Quaternion lookRotation;
+        if(ManualRotation() || AutoRotation()) {
+            ClampRotateAngle();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }else{
+            lookRotation = transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+	void UpdateFocusPoint () {
+        lastFocusPosition = focusPoint;
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0f) {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            if(focusRadius < distance) {
+                //若前后两帧距离在摄像机容忍关注点偏移的范围之外，则以距离比作插值趋向目标值
+                focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+            }
+            if(distance > 0.01f && focusCentering > 0f) {
+                //若前后两帧关注点不在同一个位置，则曲线插值修正距离
+                focusPoint = Vector3.Lerp(targetPoint, focusPoint, Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime));
+            }
+        } else {
+            focusPoint = targetPoint;
+        }
+	}
+    bool AutoRotation() {
+        if(alignDelay == 0 || Time.unscaledTime - lastManualRotationTime < alignDelay) {
+            return false;
+        }
+        Vector2 movement = new Vector2(focusPoint.x - lastFocusPosition.x, focusPoint.z - lastFocusPosition.z);
+        float movementDeltaSqr = movement.sqrMagnitude;
+        if (movementDeltaSqr < 0.00001) {
+            return false;
+        }
+        Vector2 direction = movement / Mathf.Sqrt(movementDeltaSqr);
+
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+        angle = direction.x < 0f ? 360 - angle : angle;
+        float rotationChange = alignSmoothRange * Mathf.Min(Time.unscaledDeltaTime, movementDeltaSqr);
+        float deltaAbs = Mathf.Abs(Mathf.DeltaAngle(orbitAngles.y, angle));
+        if(deltaAbs < alignSmoothRange) {
+            rotationChange *= deltaAbs / alignSmoothRange;
+        }else if(180f - deltaAbs< alignSmoothRange) {
+            rotationChange *= deltaAbs / alignSmoothRange;
+        }
+        orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, angle, rotationChange);
+        return true;
+    }
+
+    bool ManualRotation(){
+        Vector2 inputDelta = new Vector2(Input.GetAxis("Vertical Camera"), Input.GetAxis("Horizontal Camera"));
+        float e = 0.01f;
+        if(Mathf.Abs(inputDelta.x) > e || Mathf.Abs(inputDelta.y)>e) {
+            orbitAngles += inputDelta*rotationSpeed*Time.unscaledDeltaTime;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
+        }
+        return false;
+    }
+    void ClampRotateAngle() {
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, minRotateAngleX, maxRotateAngleX);
+        if (orbitAngles.y < 0f) {
+            orbitAngles.y += 360f;
+        } else if (orbitAngles.y >= 360f) {
+            orbitAngles.y -= 360f;
+        }
+    }
+}
--- Assets/Scripts/Operator/PlayerInputControl.cs
+++ Assets/Scripts/Operator/PlayerInputControl.cs
@@ -3,38 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInputControl : MonoBehaviour{
+    [SerializeField]
+    Transform playerInputSpace = default;
     ActorAnimateControl aac;
-    public int steer = 36000;
+    Rigidbody body;
     public int speed = 0;
     public int unit = 100;
+    public int jumpSpeed = 300;
+    const float gravity = -9.8f;
+    private float curUpSpeed = 0.0f;
+    private bool bJumping = false;
     private void Start() {
         aac = GetComponent<ActorAnimateControl>();
+        body = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        InputMove();
+    }
+
+    void FixedUpdate() {
+        JumpUp();
+    }
+
+    void InputMove(){
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Vector3 dir = new Vector3(x, 0, y);
-        
-        if (!Mathf.Approximately(y, 0.0f)) {
-            if (Input.GetKey(KeyCode.LeftShift)) {
-                speed = 350; //同时下shit键跑
-            } else {
-                speed = 150; //走
-            }
+        Vector3 dir = new Vector3 (x, 0f ,y);
+        if (playerInputSpace) {
+            Vector3 forward = playerInputSpace.forward;
+            Vector3 right = playerInputSpace.right;
+            right.y = forward.y = 0f;
+            dir = dir.x * right.normalized + dir.z * forward.normalized;
+        }
+        bool isRun = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);        
+        if (!bJumping && Input.GetKeyDown(KeyCode.Space)) {
+            bJumping = true;
+            curUpSpeed = jumpSpeed/unit;
+            aac.Jump();
+        }
+        if(Mathf.Abs(y)> 0.01f || Mathf.Abs(x) > 0.01f) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir, transform.up), Time.deltaTime * 10f);
+            speed = isRun?350:150; //同时下shit键跑或单纯行走
             aac.SetMoveSpeed(speed);
         } else if (speed != 0) {
             speed = 0;
             aac.SetMoveSpeed(speed);
         }
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            //speed = 0;
-            aac.Jump();
+        transform.position += dir * (speed / unit * Time.deltaTime);
+    }
+
+    void JumpUp(){
+        if(!bJumping){
+            return;
         }
-        if(!Mathf.Approximately(y, 0.0f) || !Mathf.Approximately(x, 0.0f)) {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir, transform.up), Time.deltaTime * 10f);
+        curUpSpeed = curUpSpeed + gravity * Time.deltaTime;
+        Vector3 newPos = transform.position + curUpSpeed * Time.deltaTime *transform.up;
+        if(newPos.y<=0.0f){ //这里先临时这么写吧，实际上应该以所在地块的y值为对比基准
+            bJumping = false;
+            newPos.y = 0;
+            curUpSpeed = 0;
         }
-        transform.position += transform.forward * (speed / unit * Time.deltaTime);
+        transform.position = newPos;
     }
 }
